3fd1d96b6019633b20b90e555b4c297839fb8662
shelarhitesh@outlook.com <shelarhitesh@outlook.com>
2023-08-05 12:09:58 +0000

    azure

--- manifest/main.tf
+++ manifest/main.tf
@@ -1,10 +1,24 @@
-data "kubectl_file_document" "docs" {
-    content = file("./deployment.yaml")
-}
+resource "azurerm_container_group" "example" {
+  name                = "kaexample-container"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "Public"
+  dns_name_label      = "kavyaaci-label"
+  os_type             = "Linux"
 
-resource "kubectl_manifest" "test" {
-    count       = length(data.kubectl_file_document.docs.documents)
-    yaml_body   = element(data.kubectl_file_document.docs.documents, count.index)
-    wait_for_rollout  = false
-    force_new   = true
+  container {
+    name   = "hello-world"
+    image  = "settlemint.azurecr.io/nft-bridge:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 8080
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "testing"
+  }
 }
--- manifest/provider.tf
+++ manifest/provider.tf
@@ -1,9 +1,17 @@
 terraform {
-  required_version = ">=1.0"
-  required_provider {
-     kubectl = {
-        source = "gavinbunney/kubectl"
-        version = ">= 1.7.0"
-     }
-  }
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "=0.1.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.2"   
+    }
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.16.0"
+    }
 }
