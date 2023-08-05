c323fac8348de34acb6759afb9e85cd3d12c620c
shelarhitesh@outlook.com <shelarhitesh@outlook.com>
2023-08-05 12:45:15 +0000

    changes

--- manifest/variable.tf
+++ manifest/variable.tf
@@ -4,6 +4,12 @@ variable "resource_group_location" {
   description = "Location for all resources."
 }
 
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "billingrg"
+  description = "Prefix of the resource group name that's combined with a random value so name is unique in your Azure subscription."
+}
+
 variable "resource_group_name" {
   type        = string
   default     = "billingrg"
